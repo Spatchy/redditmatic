d1096b875e06287092bca343cf297d1c3fbc11a5
Spatchy <spatchy@spatchy.net>
2021-04-09 17:04:22 +0100

    added dates of posts and comments to object

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ const r = new snoowrap({
   password: creds.reddit['password'],
 });
 
+//r.getSubreddit('askreddit').getTop({time: 'day'}).then(post => console.log(post))
+
+function convertTime (timestamp) {
+  const date = new Date(timestamp*1000);
+  return date.getDate()+
+  "/"+(date.getMonth()+1)+
+  "/"+date.getFullYear()+
+  " "+date.getHours()+
+  ":"+date.getMinutes()+
+  ":"+date.getSeconds();
+}
+
 r.getSubreddit('askreddit').getTop({time: 'day'}).map(async (post) => ({
   title: post.title, 
   author: post.author.name, 
   url: post.url,
   id: post.id,
+  date: convertTime(post.created_utc),
   comments: await post.comments.fetchMore(30, true, false).map((comment) => ({
     body: comment.body,
     author: comment.author.name,
     url: 'https://reddit.com' + comment.permalink,
     id: comment.id,
+    date: convertTime(comment.created_utc),
   }))
 }))
 .then((obj) => fs.writeFileSync('tree.json', JSON.stringify(obj, null, 4)));
