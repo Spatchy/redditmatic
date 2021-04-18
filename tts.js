8a8f33814a3ecb44f4305a2b63fd74c856151b31
Spatchy <spatchy@spatchy.net>
2021-04-19 00:11:56 +0100

    created text to speech module

---
+++ tts.js
@@ -0,0 +1,26 @@
+import textToSpeech from '@google-cloud/text-to-speech';
+import fs from 'fs';
+import util from 'util';
+
+const projID = 'redditmatic';
+const keyFile = 'redditmatic-006af59d9e4a.json';
+const client = new textToSpeech.TextToSpeechClient({projID, keyFile});
+
+export default async function runtts(text, outPath) {
+  // Construct the request
+  const request = {
+    input: {text: text},
+    voice: {
+        languageCode: "en-US",
+        name: "en-US-Wavenet-I",
+    },
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+
+  // Performs the text-to-speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile(outPath, response.audioContent, 'binary');
+  console.log('Audio content written to file: ' + outPath);
+}
