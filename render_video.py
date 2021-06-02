91fdbe10cfba5251b4eeacfdc336019b8b8ac8f5
Spatchy <spatchy@spatchy.net>
2021-06-02 22:14:03 +0100

    Implemented complete video rendering

--- render_video.py
+++ render_video.py
@@ -1,8 +1,23 @@
+import enum
 import os
-import moviepy.video.io.ImageSequenceClip
-image_folder='./out/mt7njp'
-fps=1
+from moviepy.editor import *
+import re
 
-image_files = [image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".png")]
-clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
-clip.write_videofile('my_video.mp4')
+image_folder = './out/mt7njp'
+fps = 25
+
+image_files = sorted([image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".png")], key=lambda f: int(re.sub('\D', '', f)))
+audio_files = [image_folder+'/'+aud for aud in os.listdir(image_folder) if aud.endswith(".mp3")]  # The numbers here are wrong but correspond with sorted images when left unsorted
+
+frame_buffer = []
+
+for i, image in enumerate(image_files):
+    print(image)
+    audio = AudioFileClip(audio_files[i])
+    clip = ImageClip(image).set_duration(audio.duration)
+    clip = clip.set_audio(audio)
+    clip.fps = fps
+    frame_buffer.append(clip)
+
+render = concatenate_videoclips(frame_buffer)
+render.write_videofile('my_video.mp4')
