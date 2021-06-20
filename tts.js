7854b3cb458eb6bc51c61b5f0b79dc2e7f377e7e
Spatchy <spatchy@spatchy.net>
2021-06-21 00:55:47 +0100

    tts now uses the deacronym engine

--- tts.js
+++ tts.js
@@ -1,15 +1,17 @@
 import textToSpeech from '@google-cloud/text-to-speech';
 import fs from 'fs';
 import util from 'util';
+import deacronym from './deacronym.js'
 
 const projID = 'redditmatic';
 const keyFile = 'redditmatic-006af59d9e4a.json';
 const client = new textToSpeech.TextToSpeechClient({projID, keyFile});
 
 export default async function runtts(text, outPath) {
+  const tweakedText = deacronym(text);
   // Construct the request
   const request = {
-    input: {text: text},
+    input: {text: tweakedText},
     voice: {
         languageCode: "en-US",
         name: "en-US-Wavenet-I",
