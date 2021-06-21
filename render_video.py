e0d7b94c955855165b8ab05ba717f15e0e9911ad
Spatchy <spatchy@spatchy.net>
2021-06-22 00:47:08 +0100

    implemented background music

--- render_video.py
+++ render_video.py
@@ -2,6 +2,7 @@ import enum
 import os
 from moviepy.editor import *
 import re
+import random
 
 total_duration = 0
 
@@ -22,6 +23,27 @@ def makeVideo(frame_buffer, image_folder):
         clip.fps = fps
         frame_buffer.append(clip)
 
+
+def assemble_bgm():
+    global total_duration
+    bgm_duration = 0
+
+    selected_tracks = []
+    audio_files = ['./bgm/'+aud for aud in os.listdir('./bgm') if aud.endswith(".wav")]
+    while bgm_duration < total_duration:
+        choice = random.choice(audio_files)
+        print(choice)
+        clip = AudioFileClip(choice).set_start(bgm_duration)
+        bgm_duration += clip.duration
+        selected_tracks.append(clip)
+        audio_files.remove(choice)  # so same track is not selected twice
+    
+    bgm = CompositeAudioClip(selected_tracks)
+    bgm = bgm.set_end(total_duration).set_duration(total_duration)
+    bgm = bgm.set_fps(44100).audio_normalize().volumex(0.15).audio_fadein(4).audio_fadeout(2)
+
+    return bgm
+
 frame_buffer = []
 print("assembling video", flush = True)
 for dir in next(os.walk('./out'))[1]:
@@ -33,5 +55,7 @@ for dir in next(os.walk('./out'))[1]:
     print("FINISHED " + dir, flush = True)
 print("ASSEMBLY COMPLETE, STARTING RENDER", flush = True)
 render = concatenate_videoclips(frame_buffer)
+bgm = assemble_bgm()
+render = render.without_audio().set_audio(CompositeAudioClip([render.audio, bgm]))
 render.write_videofile('my_video.mp4')
 print("---RENDER COMPLETE!---", flush = True)
